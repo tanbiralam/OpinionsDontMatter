2f9a01849ad56583a4c40eda9d2d993647e78d74
tanbiralam <itzmetanbir@gmail.com>
2024-06-25 23:57:40 +0530

    ui ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import {ThemeContext} from "./context/ThemeContext";
+import { ThemeContext } from "./context/ThemeContext";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import NewIdea from "./components/NewIdea";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Hero/>
+    element: <Hero />,
   },
   {
     path: "/new",
-    element: <NewIdea/>
+    element: <NewIdea />,
   },
-
-])
+]);
 
 const App = () => {
-  const [theme, setTheme] = useState('winter');
+  const [theme, setTheme] = useState("winter");
 
   return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
-      <div className="flex flex-col items-center p-4 md:p-10" data-theme={theme}>
-        <div className="max-w-2xl w-full items-center">
-          <RouterProvider router={router}/>
-          
+    <>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <Header />
+        <div
+          className="flex flex-col items-center p-4 md:p-10"
+          data-theme={theme}
+        >
+          <div className="max-w-2xl w-full items-center">
+            <RouterProvider router={router} />
+          </div>
         </div>
-      </div>
-    </ThemeContext.Provider>
+      </ThemeContext.Provider>
+    </>
   );
 };
 
--- src/components/NewIdea.jsx
+++ src/components/NewIdea.jsx
@@ -1,7 +1,46 @@
+import { ChevronLeft, Info, Send } from "lucide-react";
+
 const NewIdea = () => {
   return (
-    <div>NewIdea</div>
-  )
-}
+    <div>
+      <button className="btn mt-7">
+        <ChevronLeft />
+        Back
+      </button>
+      <h2 className="text-2xl font-bold mt-5">
+        Free Upload Your Best Prompt that you used
+      </h2>
 
-export default NewIdea
+      <div className="flex flex-col mt-7 gap-2">
+        <label htmlFor="" className="font-bold">
+          Your Pompt
+        </label>
+        <textarea
+          className="border-primary textarea textarea-bordered"
+          placeholder="prompt"
+        ></textarea>
+      </div>
+      <div className="flex flex-col mt-7 gap-2">
+        <label htmlFor="" className="flex justify-between font-bold">
+          Your Name
+          <span className="flex items-center gap-2 text-sm font-semibold">
+            <Info className="w-4 h-4" />
+            No Account Needed
+          </span>
+        </label>
+        <input
+          type="text"
+          placeholder="your name"
+          className="border-primary input w-full"
+        />
+      </div>
+
+      <button className="btn w-full btn-primary mt-7">
+        <Send className="h-4 w-4" />
+        Send
+      </button>
+    </div>
+  );
+};
+
+export default NewIdea;
